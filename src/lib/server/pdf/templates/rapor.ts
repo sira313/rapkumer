5a53af52a6e3c69bab3d2d45a943e1bb5b16d35d
sira313 <me@apoxi.cam>
2026-06-12 18:00:12 +0700

    chore: center signature

--- src/lib/server/pdf/templates/rapor.ts
+++ src/lib/server/pdf/templates/rapor.ts
@@ -500,8 +500,8 @@ ${
 <div class="no-break" style="margin-top:12pt;">
 	<table class="ttd-table">
 		<colgroup>
-			<col style="width:40%;">
-			<col style="width:60%;">
+			<col style="width:50%;">
+			<col style="width:50%;">
 		</colgroup>
 		<tbody>
 			<tr>
